25257f8a7f668e4c42027061e21b5e06d4dbb027
y_fukuda <colors92window@gmail.com>
2020-10-21 07:02:56 +0900

    Dollars should be equal iff they have same amount

--- money/dollar.py
+++ money/dollar.py
@@ -1,6 +1,11 @@
+from __future__ import annotations
+
 class Dollar(object):
     def __init__(self, amount: int):
         self.amount = amount
 
-    def times(self, multiplier: int):
+    def times(self, multiplier: int) -> Dollar:
         return Dollar(self.amount * multiplier)
+
+    def equals(self, dollar: Dollar):
+        return self.amount == dollar.amount
--- test/money_test.py
+++ test/money_test.py
@@ -3,11 +3,17 @@ from money.dollar import Dollar
 
 class TestMoney(unittest.TestCase):
     def test_multiplication(self):
+        "Multiplication of Dollars should return new Dollar object of multiplied amount."
         five = Dollar(5)
         product = five.times(2)
         self.assertEqual(10, product.amount)
         product = five.times(3)
         self.assertEqual(15, product.amount)
+
+    def test_equality(self):
+        "Dollars should be equal iff they have same amount."
+        self.assertTrue(Dollar(5).equals(Dollar(5)))
+        self.assertFalse(Dollar(5).equals(Dollar(6)))
 
 
 if __name__ == "__main__":
